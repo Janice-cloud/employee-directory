08c0e97a97d196b10e66d8eb7b70b2b417b00387
Janice Leung <janice.leung2008@gmail.com>
2020-06-30 16:55:14 +1000

    add json

--- Employee-Directory/src/components/Table/index.js
+++ Employee-Directory/src/components/Table/index.js
@@ -1,43 +1,27 @@
 import React from "react";
 
-
 function Table(props) {
     return (
         <table class="table">
         <thead>
           <tr>
-            <th onClick={() => sortFunc("name")}><Button>First Name</Button></th>
-            <th>Lastname</th>
+            <th>First Name</th>
+            <th>Last Name</th>
             <th>Gender</th>
             <th>Email</th>
             <th>Phone Number</th>
-            <th>Age</th>
+            <th>Country</th>
+            <th>Department</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>Male</td>
-            <td>john@example.com</td>
-            <td>02 9438 4837</td>
-            <td>19</td>
-          </tr>
-          <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>Female</td>
-            <td>mary@example.com</td>
-            <td>03 9438 4838</td>
-            <td>25</td>
-          </tr>
-          <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>Female</td>
-            <td>july@example.com</td>
-            <td>03 9438 4839</td>
-            <td>30</td>
+            <td>{props.first_name}</td>
+            <td>{props.last_name}</td>
+            <td>{props.gender}</td>
+            <td>{props.email}</td>
+            <td>{props.phone}</td>
+            <td>{props.department}</td>
           </tr>
         </tbody>
       </table>
